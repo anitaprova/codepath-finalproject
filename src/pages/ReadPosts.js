bc0fdd719cd2c36437a027d8b8d7cde6fc4215c3
anitaprova <104943181+anitaprova@users.noreply.github.com>
2024-04-20 16:34:08 -0400

    added order by buttons

--- src/pages/ReadPosts.js
+++ src/pages/ReadPosts.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { supabase } from "../client";
 import { Link } from "react-router-dom";
-import Card from '../components/Card';
+import Card from "../components/Card";
 
 const ReadPosts = ({ props, searchQuery }) => {
   const [posts, setPosts] = useState([]);
@@ -20,29 +20,49 @@ const ReadPosts = ({ props, searchQuery }) => {
     post.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  const mostPopularPosts = () => {
+    const sortedPosts = [...filteredPosts].sort((a, b) => b.likes - a.likes);
+    setPosts(sortedPosts);
+  };
+
+  const newestPosts = () => {
+    const sortedPosts = [...filteredPosts].sort((a, b) => {
+      const dateA = new Date(a.created_at);
+      const dateB = new Date(b.created_at);
+      return dateB - dateA;
+    });
+    setPosts(sortedPosts);
+  };
+  
   return (
-    <div className="ReadPosts">
-      {filteredPosts && filteredPosts.length > 0 ? (
-        filteredPosts.map((post, index) => (
-          <Card
-            key={index}
-            id={post.id}
-            name={post.name}
-            title={post.title}
-            description={post.description}
-            game={post.game}
-            likes={post.likes}
-          />
-        ))
-      ) : (
-        <div>
-          <h2>Your Gallery</h2>
-          <p>You haven't a post yet</p>
-          <Link to="/new">
-            <button className="headerBtn"> Create a Post </button>
-          </Link>
-        </div>
-      )}
+    <div>
+      <p>
+        Order By: <button onClick={newestPosts}>Newest</button>{" "}
+        <button onClick={mostPopularPosts}>Most popular</button>
+      </p>
+      <div className="ReadPosts">
+        {filteredPosts && filteredPosts.length > 0 ? (
+          filteredPosts.map((post, index) => (
+            <Card
+              key={index}
+              id={post.id}
+              name={post.name}
+              title={post.title}
+              description={post.description}
+              game={post.game}
+              likes={post.likes}
+            />
+          ))
+        ) : (
+          <div>
+            <h2>Your Gallery</h2>
+            <p>You haven't a post yet</p>
+            <Link to="/new">
+              <button className="headerBtn"> Create a Post </button>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
